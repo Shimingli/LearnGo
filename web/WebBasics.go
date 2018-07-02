d8dd9f978431e681ff97160b15054d0ee6a73d6a
win7 <737141437@qq.com>
2018-07-02 18:55:50 +0800

    1、提交Web的详解

---
+++ web/WebBasics.go
@@ -0,0 +1,14 @@
+package web
+
+func init() {
+	// 如何使用 go来编写 web应用，go目前已经有了成熟的http处理包，这使得编写能够做任何事情的动态的Web程序易于反掌
+	// 1、web的工作的方式
+	//2、Go搭建一个简单的web服务
+	//3、go如何使得微博工作
+	//4、go的http包详解
+	//5、小结
+
+//     todo    https://www.jianshu.com/p/84aa55a8a7eb
+
+}
+
