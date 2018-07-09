336ae8beb8c92f2c0fa8124c1b8abab1eeeb8c6a
win7 <737141437@qq.com>
2018-07-09 15:24:09 +0800

    1 db Demo

---
+++ db/GoDemoSQLite3.go
@@ -0,0 +1,73 @@
+package main
+
+
+import (
+	_ "github.com/mattn/go-sqlite3"
+	"database/sql"
+	"fmt"
+	"time"
+)
+
+func main() {
+	//SQLite 是一个开源的嵌入式关系数据库，实现自包容、零配置、支持事务的SQL数据库引擎。其特点是高度便携、使用方便、结构紧凑、高效、可靠。 与其他数据库管理系统不同，SQLite 的安装和运行非常简单，在大多数情况下,只要确保SQLite的二进制文件存在即可开始创建、连接和使用数据库。如果您正在寻找一个嵌入式数据库项目或解决方案，SQLite是绝对值得考虑。SQLite可以是说开源的Access。
+
+	//Go支持sqlite的驱动也比较多，但是好多都是不支持database/sql接口的
+//https://github.com/mattn/go-sqlite3 支持database/sql接口，基于cgo写的
+	db, err := sql.Open("sqlite3", "./foo.db")
+	checkErr(err)
+
+	//插入数据
+	stmt, err := db.Prepare("INSERT INTO userinfo(username, department, created) values(?,?,?)")
+	checkErr(err)
+
+	res, err := stmt.Exec("astaxie", "研发部门", "2012-12-09")
+	checkErr(err)
+
+	id, err := res.LastInsertId()
+	checkErr(err)
+
+	fmt.Println(id)
+	//更新数据
+	stmt, err = db.Prepare("update userinfo set username=? where uid=?")
+	checkErr(err)
+
+	res, err = stmt.Exec("astaxieupdate", id)
+	checkErr(err)
+
+	affect, err := res.RowsAffected()
+	checkErr(err)
+
+	fmt.Println(affect)
+
+	//查询数据
+	rows, err := db.Query("SELECT * FROM userinfo")
+	checkErr(err)
+
+	for rows.Next() {
+		var uid int
+		var username string
+		var department string
+		var created time.Time
+		err = rows.Scan(&uid, &username, &department, &created)
+		checkErr(err)
+		fmt.Println(uid)
+		fmt.Println(username)
+		fmt.Println(department)
+		fmt.Println(created)
+	}
+
+	//删除数据
+	stmt, err = db.Prepare("delete from userinfo where uid=?")
+	checkErr(err)
+
+	res, err = stmt.Exec(id)
+	checkErr(err)
+
+	affect, err = res.RowsAffected()
+	checkErr(err)
+
+	fmt.Println(affect)
+
+	db.Close()
+
+}
